57b69e45b002f68d53c589a5ff4cc7c389ed5bee
Sungjin Mo <samuelmo78@gmail.com>
2019-12-23 23:49:37 -0600

    a

---
+++ app/src/main/java/com/rjt/property_management/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.rjt.property_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //
+        // change
+    }
+}
